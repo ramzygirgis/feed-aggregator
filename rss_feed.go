ddba5f912c0d4148b976942fcd6aa0412b991a64
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-05-18 13:55:50 -0400

    rss feed unmarshaling

--- rss_feed.go
+++ rss_feed.go
@@ -5,37 +5,64 @@ import (
 	"net/http"
 	"io"
 	"encoding/xml"
+	"html"
 )
 
 type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
 
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+
+func (r *RSSFeed) decodeHTML() {
+	r.Channel.Title = html.UnescapeString(r.Channel.Title)
+	r.Channel.Description = html.UnescapeString(r.Channel.Description)
+	for i := 0; i < len(r.Channel.Item); i++ {
+		r.Channel.Item[i].Title = html.UnescapeString(r.Channel.Item[i].Title)
+		r.Channel.Item[i].Description = html.UnescapeString(r.Channel.Item[i].Description)
+	}
 }
 
 
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
-	req, err := http.NewRequestWithContext(ctx feedURL, http.MethodGet)
+	req, err := http.NewRequestWithContext(ctx, feedURL, http.MethodGet)
 	if err != nil {
-		return &RSSFeed{}, err
+		return nil, err
 	}
 	req.Header.Set("User-Agent", "gator")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return &RSSFeed{}, err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return &RSSFeed{}, err
+		return nil, err
 	}
 
 	var data RSSFeed
 	err = xml.Unmarshal(bodyBytes, &data)
 	if err != nil {
-		return &RSSFeed{}, err
+		return nil, err
 	}
+
+	data.decodeHTML()
 
 	return &data, nil
 }
+
+
