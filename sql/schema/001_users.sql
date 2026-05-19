2836c6e80ef868aa032a0cf269a4cb059849af41
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-05-18 21:36:52 -0400

    new table: feeds

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
   id UUID PRIMARY KEY,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
-  name VARCHAR(1000) unique NOT NULL
+  name TEXT unique NOT NULL
 );
 
 -- +goose Down
