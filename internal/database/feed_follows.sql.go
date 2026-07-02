699b85713456da4041c8ba091fd0db8e224ec912
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-07-02 17:26:20 -0400

    added query for removing from feed_follows table

--- internal/database/feed_follows.sql.go
+++ internal/database/feed_follows.sql.go
@@ -74,6 +74,21 @@ func (q *Queries) CreateFeedFollow(ctx context.Context, arg CreateFeedFollowPara
 	return i, err
 }
 
+const deleteFeedFollow = `-- name: DeleteFeedFollow :exec
+DELETE FROM feed_follows
+WHERE user_id = $1 AND feed_id = $2
+`
+
+type DeleteFeedFollowParams struct {
+	UserID uuid.UUID
+	FeedID uuid.UUID
+}
+
+func (q *Queries) DeleteFeedFollow(ctx context.Context, arg DeleteFeedFollowParams) error {
+	_, err := q.db.ExecContext(ctx, deleteFeedFollow, arg.UserID, arg.FeedID)
+	return err
+}
+
 const getFeedFollowsForUser = `-- name: GetFeedFollowsForUser :many
 SELECT id, created_at, updated_at, user_id, feed_id -- potentially select less later
 FROM feed_follows
--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -26,3 +26,8 @@ ON feeds.id = feed_id;
 SELECT * -- potentially select less later
 FROM feed_follows
 WHERE user_id = $1; 
+
+
+-- name: DeleteFeedFollow :exec
+DELETE FROM feed_follows
+WHERE user_id = $1 AND feed_id = $2;
