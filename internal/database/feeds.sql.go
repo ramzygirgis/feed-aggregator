79861b0428cf805857c4975c8bfade62f1205927
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-07-02 18:20:40 -0400

    GetNextFeedToFetch query

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -134,6 +134,28 @@ func (q *Queries) GetFeeds(ctx context.Context) ([]Feed, error) {
 	return items, nil
 }
 
+const getNextFeedToFetch = `-- name: GetNextFeedToFetch :one
+SELECT id, created_at, updated_at, name, url, user_id, last_fetched_at
+FROM feeds
+ORDER BY last_fetched_at NULLS FIRST
+LIMIT 1
+`
+
+func (q *Queries) GetNextFeedToFetch(ctx context.Context) (Feed, error) {
+	row := q.db.QueryRowContext(ctx, getNextFeedToFetch)
+	var i Feed
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Name,
+		&i.Url,
+		&i.UserID,
+		&i.LastFetchedAt,
+	)
+	return i, err
+}
+
 const markFeedFetched = `-- name: MarkFeedFetched :exec
 UPDATE feeds
 SET 
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -30,6 +30,13 @@ WHERE id = $1;
 UPDATE feeds
 SET 
   last_fetched_at = NOW(),
-  last_updated_at = NOW()
+  updated_at = NOW()
 WHERE id = $1;
+
+
+-- name: GetNextFeedToFetch :one
+SELECT *
+FROM feeds
+ORDER BY last_fetched_at NULLS FIRST
+LIMIT 1;
 
