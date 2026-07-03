b3e9eaeec17bb2658968a6b67f196c2c811d5ee9
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-07-03 09:46:38 -0400

    get posts

--- internal/database/posts.sql.go
+++ internal/database/posts.sql.go
@@ -63,3 +63,42 @@ func (q *Queries) CreatePost(ctx context.Context, arg CreatePostParams) (Post, e
 	)
 	return i, err
 }
+
+const getPosts = `-- name: GetPosts :many
+SELECT id, created_at, updated_at, title, url, description, published_at, feed_id
+FROM posts
+ORDER BY created_at DESC
+LIMIT $1
+`
+
+func (q *Queries) GetPosts(ctx context.Context, limit int32) ([]Post, error) {
+	rows, err := q.db.QueryContext(ctx, getPosts, limit)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Post
+	for rows.Next() {
+		var i Post
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.Title,
+			&i.Url,
+			&i.Description,
+			&i.PublishedAt,
+			&i.FeedID,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -11,3 +11,10 @@ VALUES (
     $8
 )
 RETURNING *;
+
+
+-- name: GetPosts :many
+SELECT *
+FROM posts
+ORDER BY created_at DESC
+LIMIT $1;
